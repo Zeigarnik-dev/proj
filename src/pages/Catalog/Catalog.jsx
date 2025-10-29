b7f3bfa659b92d50e70369759c610b9ab8fed553
Zeigarnik-dev <brajlovskijroman23@gmail.com>
2025-10-29 12:00:48 +0300

    Add files via upload

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,5 +1,4 @@
 import Title from "../../components/Title/Title";
-import Button from "../../components/Button/Button";
 import s from "./Catalog.module.css";
 import Filter from "../../components/Filter/Filter";
 import { cards, filters } from "../../data.js";
@@ -7,17 +6,12 @@ import Card from "../../components/Card/Card";
 import { useState } from "react";
 export default function Catalog() {
   let[search, findItem] = useState("")
-  let[selected, setSelect] = useState('0')
+  let[selected, setSelect] = useState('default')
   let[category, setCategory] = useState(null)
   // let searchedProd = cards.filter(el=> el.title.toLowerCase().includes(search.toLowerCase()))
   // let selectedProd = cards.filter(el=>Number(el.price.split(' ')[0]) > Number(selected))
   let filteredCards = cards.filter(el=>{
-    //поиск
     const matchesSearch = el.title.toLowerCase().includes(search.toLowerCase())
-    //фильтрация
-    const price = el.price.split(' ')[0]
-    const matchPrice = Number(price) > Number(selected) || selected === '0'
-    //категории
     let matchesCategory = true;
     if(category !== null){
       const actf = filters.find(el=>el.id === category)
@@ -25,14 +19,25 @@ export default function Catalog() {
         matchesCategory = el.category.toLowerCase().includes(actf.txt.toLowerCase())
       }
     }
-    return matchesSearch && matchPrice && matchesCategory
+    return matchesSearch && matchesCategory
   })
+
+const sortprod = (sorting, arr)=>{
+  switch(sorting){
+    case "ToHight": return [...arr].sort((a, b)=> Number(a.price.split(' ')[0])-Number(b.price.split(' ')[0]))
+    case "ToLow": return [...arr].sort((a, b)=> Number(b.price.split(' ')[0])-Number(a.price.split(' ')[0]))
+    default: return arr
+  }
+}
+
+const finProds = sortprod(selected, filteredCards)
+console.log(finProds)
 
   return (
     <section>
       <div className="container">
         <div className={s.tit_serch}>
-          <Title txt="Каталог" cl />
+          <Title txt="Каталог"/>
           <form className={s.search}>
             <input type="text" onChange={(e)=>findItem(e.target.value)} />
             <img src="/Frame.svg" alt="" className={s.lope} />
@@ -40,22 +45,21 @@ export default function Catalog() {
         </div>
         <div className={s.sort_txt}>
           <p className={s.p}>Выберите категорию, если это необходимо</p>
-          <select name="" id="" value={selected} onChange={(e)=>setSelect(e.target.value)}>
-            <option value="0">
+          <select value={selected} onChange={(e)=>setSelect(e.target.value)}>
+            <option value="default">
               без фильтра
             </option>
-            <option value="3000">&gt; 3000</option>
-            <option value="3500">&gt; 3500</option>
-            <option value="4000">&gt; 4000</option>
+            <option value="ToHight">По возрастанию</option>
+            <option value="ToLow">По убыванию</option>
           </select>
         </div>
         <div className={s.filters}>
           <Filter txt="Все букеты" isFirst={category === null} handleClick={()=> setCategory(null)}/>
-          {filters.map((filter)=>(<Filter key={filter.id} txt={filter.txt} isFirst={category === filter.id} handleClick={()=> setCategory(filter.id)}/>))}
+          {filters.map((filter)=>(<Filter txt={filter.txt} isFirst={category === filter.id} handleClick={()=> setCategory(filter.id)}/>))}
         </div>
         <div className={s.catalog}>
           { 
-          filteredCards.length ? filteredCards.map((card) => (<Card {...card} />)): <p>Товары по запросу "{search}" не найдены</p>
+          finProds.length ? finProds.map((card) => (<Card {...card} />)): <p>Товары по запросу "{search}" не найдены</p>
           }
         </div>
       </div>
